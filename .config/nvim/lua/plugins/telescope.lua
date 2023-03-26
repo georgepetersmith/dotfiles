08c502af1483a78a54c7d2298b9e1a970f951559
George Smith <georgepetersmith@outlook.com>
2023-03-26 00:07:20 +0000

    Align mapping with Helix

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,19 +6,17 @@ return {
     dependencies = { {'nvim-lua/plenary.nvim'} },
     keys = {
       { '<leader>pf' },
-      { '<leader>ps' },
-      { '<leader>pk' },
-      { '<leader>pb' },
-      { 'C-p' },
+      { '<leader>f' },
+      { '<leader>b' },
+      { '<leader>?' },
+      { '<leader>/' },
     },
     config = function()
       local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>pk', builtin.keymaps, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-          builtin.grep_string({ search = vim.fn.input('Grep > ')});
-        end)
+        vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>?', builtin.keymaps, {})
+        vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
     end,
   }
--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -6,3 +6,5 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 vim.keymap.set('x', '<leader>p', "\"_dP")
+
+vim.keymap.set('n', '<leader>w', '<C-w>')
