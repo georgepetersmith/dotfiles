ff9bb0c679bbb2bc929183157478f78f6320e8ee
George Smith <georgepetersmith@outlook.com>
2023-04-02 19:47:45 +0100

    Fix cmp selection mapping

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -24,14 +24,67 @@ return {
   },
   config = function()
     local lsp = require('lsp-zero')
+
+    lsp.preset("recommended")
+
+    lsp.ensure_installed({
+      'tsserver',
+      'rust_analyzer',
+    })
+
+    local cmp = require('cmp')
+    cmp.setup({
+      sources = {
+        {name = 'path'},
+        {name = 'nvim_lsp'},
+        {name = 'nvim_lsp_signature_help'},
+        {name = 'buffer', keyword_length = 3},
+        {name = 'luasnip', keyword_length = 2},
+      }
+    })
+
+    local cmp_select = {behavior = cmp.SelectBehavior.Select}
+    local cmp_mappings = lsp.defaults.cmp_mappings({
+      ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+      ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+      ["<C-Space>"] = cmp.mapping.complete(),
+    })
+
+    cmp_mappings['<Tab>'] = nil
+    cmp_mappings['<S-Tab>'] = nil
+
+    lsp.setup_nvim_cmp({
+      mapping = cmp_mappings
+    })
+
+    lsp.set_preferences({
+      suggest_lsp_servers = false,
+      sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+      }
+    })
+
     lsp.on_attach(function(client, bufnr)
-      lsp.default_keymaps({buffer = bufnr})
+      local opts = {buffer = bufnr, remap = false}
+
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
       --[[ There is currently a bug with Roslyn that is breaking the LSP contract and causing the following error:
-
       Error executing vim.schedule lua callbaack:
       /usr/share/[...]/semantic_tokens.lua:342:E5248: Invalid character in group name.
-
       https://nicolaiarocci.com/making-csharp-and-omnisharp-play-well-with-neovim/ ]]
       if client.name == "omnisharp" then
         client.server_capabilities.semanticTokensProvider = {
@@ -109,7 +162,10 @@ return {
           range = true,
         }
       end
+
+
     end)
+
 
     local lspconfig = require('lspconfig')
     -- (Optional) Configure lua language server for neovim
@@ -117,60 +173,19 @@ return {
 
     lspconfig.omnisharp.setup {
       cmd = { "dotnet", "/Users/george/.omnisharp/OmniSharp.dll", "--languageserver" },
-
-      -- Enables support for reading code style, naming convention and analyzer
-      -- settings from .editorconfig.
       enable_editorconfig_support = true,
-
-      -- If true, MSBuild project system will only load projects for files that
-      -- were opened in the editor. This setting is useful for big C# codebases
-      -- and allows for faster initialization of code navigation features only
-      -- for projects that are relevant to code that is being edited. With this
-      -- setting enabled OmniSharp may load fewer projects and may thus display
-      -- incomplete reference lists for symbols.
       enable_ms_build_load_projects_on_demand = false,
-
-      -- Enables support for roslyn analyzers, code fixes and rulesets.
       enable_roslyn_analyzers = false,
-
-      -- Specifies whether 'using' directives should be grouped and sorted during
-      -- document formatting.
       organize_imports_on_format = false,
-
-      -- Enables support for showing unimported types and unimported extension
-      -- methods in completion lists. When committed, the appropriate using
-      -- directive will be added at the top of the current file. This option can
-      -- have a negative impact on initial completion responsiveness,
-      -- particularly for the first few completion sessions after opening a
-      -- solution.
       enable_import_completion = false,
-
-      -- Specifies whether to include preview versions of the .NET SDK when
-      -- determining which version to use for project loading.
       sdk_include_prereleases = true,
-
-      -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-      -- true
       analyze_open_documents_only = false,
     }
 
     lsp.setup()
 
-    local cmp = require('cmp')
-    local cmp_action = require('lsp-zero').cmp_action()
-
-    cmp.setup({
-      sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lsp_signature_help'},
-        {name = 'buffer', keyword_length = 3},
-        {name = 'luasnip', keyword_length = 2},
-      },
-      mapping = {
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-      }
+    vim.diagnostic.config({
+      virtual_text = true
     })
 
   end
