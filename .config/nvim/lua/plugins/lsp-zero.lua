d011a1b35f1d04352216a1c007ae4e859814e387
George Smith <georgepetersmith@outlook.com>
2023-04-10 12:41:45 +0100

    Update diagnostics on insert

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -23,6 +23,22 @@ return {
     {'rafamadriz/friendly-snippets'}, -- Optional
   },
   config = function()
+
+
+    vim.diagnostic.config({
+      virtual_text = false,
+      signs = true,
+      update_in_insert = true,
+      underline = true,
+      severity_sort = false,
+      float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+      },
+    })
+
     local lsp = require('lsp-zero').preset({})
 
     lsp.on_attach(function(client, bufnr)
@@ -37,13 +53,13 @@ return {
 
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
       vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
       vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-      vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-      vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>n", function() vim.lsp.buf.rename() end, opts)
       vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
       --[[ There is currently a bug with Roslyn that is breaking the LSP contract and causing the following error:
