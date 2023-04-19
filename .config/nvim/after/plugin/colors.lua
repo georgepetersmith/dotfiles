cbe2a4690f975d832450b09cfdd73ce0148a33c1
George Smith <georgepetersmith@outlook.com>
2023-04-19 21:23:55 +0100

    Replace catppuccin theme with gruvbox dark

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,9 +1,13 @@
 function ColorMyPencils(color)
-	color = color or 'catppuccin'
+	color = color or 'gruvbox'
 	vim.cmd.colorscheme(color)
+  vim.o.background = "dark"
+
+  vim.g.gruvbox_improved_strings = 1
+  vim.g.gruvbox_improved_warnings = 1
 
 	-- vim.api.nvim_set_hl(0, 'Normal', {bg = 'none'})
 	-- vim.api.nvim_set_hl(0, 'NormalFloat', {bg = 'none'})
 end
 
-ColorMyPencils("catppuccin-mocha")
+ColorMyPencils("gruvbox")
