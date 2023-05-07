ed825db77fc6a24c0d330a639587da14947f3114
George Smith <georgepetersmith@outlook.com>
2023-05-07 21:54:54 +0100

    Fix operator background for gruvbox

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,13 +1,12 @@
 function ColorMyPencils(color)
-	color = color or 'gruvbox'
-	vim.cmd.colorscheme(color)
-  vim.o.background = "dark"
+    color = color or 'gruvbox'
+    vim.cmd.colorscheme(color)
+    vim.o.background = "dark"
 
-  vim.g.gruvbox_improved_strings = 1
-  vim.g.gruvbox_improved_warnings = 1
+    vim.g.gruvbox_improved_strings = 1
+    vim.g.gruvbox_improved_warnings = 1
 
-	-- vim.api.nvim_set_hl(0, 'Normal', {bg = 'none'})
-	-- vim.api.nvim_set_hl(0, 'NormalFloat', {bg = 'none'})
+    vim.api.nvim_set_hl(0, 'Operator', {bg = 'none'})
 end
 
 ColorMyPencils("gruvbox")
