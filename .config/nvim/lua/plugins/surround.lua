9931310a9bed7ab0ee2b02bf7fc38faaf4feae6a
George Smith <georgepetersmith@outlook.com>
2023-05-07 21:55:30 +0100

    Add call to setup function

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -2,5 +2,7 @@ return {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
-    --config = true
+    config = function()
+        require("nvim-surround").setup({})
+    end
 }
