57fe50539be96ccbd44500d16d6345bf59425a79
George Smith <georgepetersmith@outlook.com>
2023-06-01 23:27:41 +0100

    Update telescope methods

--- .config/nvim/lua/plugins/lsp-zero.lua
+++ .config/nvim/lua/plugins/lsp-zero.lua
@@ -72,7 +72,7 @@ return {
 
       --vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       --vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-      --vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
       --vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
       vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,39 +1,51 @@
 return {
     'nvim-telescope/telescope.nvim',
-    lazy = true,
     cmd = 'Telescope',
+    lazy = false,
     tag = '0.1.1',
     dependencies = { {'nvim-lua/plenary.nvim'} },
-    keys = {
-      { '<leader>pf' },
-      { '<leader>f' },
-      { '<leader>b' },
-      { '<leader>?' },
-      { '<leader>/' },
-    },
     opts = {
       file_ignore_patterns = { "node_modules" },
-      path_display = { "truncate" },
+      -- path_display = { "truncate" },
       winblend = 0,
       border = {},
       borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
       color_devicons = true,
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        }
+      }
     },
     config = function()
       local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>?', builtin.keymaps, {})
-        vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>D', builtin.diagnostics, {})
-        vim.keymap.set('n', '<leader>r', builtin.resume, {})
-        vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, {})
-        vim.keymap.set('n', '<leader>S', builtin.lsp_dynamic_workspace_symbols, {})
-        vim.keymap.set('n', '<leader>a', builtin.quickfix, {})
-        vim.keymap.set('n', 'gr', builtin.lsp_references, {})
-        vim.keymap.set('n', 'gd', builtin.lsp_type_definitions, {})
-        vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
+
+      local custom = {}
+      custom.project_files = function()
+        local opts = {} -- define here if you want to define something
+        vim.fn.system('git rev-parse --is-inside-work-tree')
+        if vim.v.shell_error == 0 then
+          require"telescope.builtin".git_files(opts)
+        else
+          require"telescope.builtin".find_files(opts)
+        end
+      end
+
+      vim.keymap.set('n', '<leader>f', custom.project_files, {})
+      vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>?', builtin.keymaps, {})
+      vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>D', builtin.diagnostics, {})
+      vim.keymap.set('n', '<leader>r', builtin.resume, {})
+      vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, {})
+      vim.keymap.set('n', '<leader>S', builtin.lsp_dynamic_workspace_symbols, {})
+      vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+      vim.keymap.set('n', 'gd', builtin.lsp_type_definitions, {})
+      vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
 
       -- To get fzf loaded and working with telescope, you need to call
       -- load_extension, somewhere after setup function:
