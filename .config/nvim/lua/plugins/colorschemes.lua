ae48328440ac0a3ae3361dc3dd284f84077dea95
George Smith <georgepetersmith@outlook.com>
2024-03-08 10:47:09 +0000

    Add solarized-osaka

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,14 +1,25 @@
 return {
-    'loctvl842/monokai-pro.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    priority = 1000,
-    config = function()
-        require("monokai-pro").setup({
-            transparent_background = true,
-            terminal_colors = true,
-            devicons = true
-        })
+    {
+        'loctvl842/monokai-pro.nvim',
+        enabled = false,
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        priority = 1000,
+        config = function()
+            require("monokai-pro").setup({
+                transparent_background = true,
+                terminal_colors = true,
+                devicons = true
+            })
 
-        vim.cmd([[colorscheme monokai-pro]])
-    end
+            vim.cmd([[colorscheme monokai-pro]])
+        end
+    },
+    {
+        "craftzdog/solarized-osaka.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function ()
+            vim.cmd([[colorscheme solarized-osaka]])
+        end
+    }
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
+        { '<leader>F', '<cmd>Telescope git_files<cr>' },
         { '<leader>f', '<cmd>Telescope find_files<cr>' },
         { '<leader>/', '<cmd>Telescope live_grep<cr>' },
         { '<leader>b', '<cmd>Telescope buffers<cr>' },
@@ -13,7 +14,17 @@ return {
             path_display = {
                 'truncate',
                 truncate = 3
+            }
+        },
+        pickers = {
+            find_files = {
+                theme = "dropdown",
+                previewer = false,
             },
-        }
+            git_files = {
+                theme = "dropdown",
+                previewer = false,
+            }
+        },
     }
 }
