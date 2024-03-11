7c17a98ccfd3c48e3968f07ea732d7d8096adf33
George Smith <georgepetersmith@outlook.com>
2024-03-11 09:15:11 +0000

    Add catppuccin colour scheme

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -2,11 +2,12 @@ return {
     {
         'loctvl842/monokai-pro.nvim',
         enabled = false,
+        lazy = false,
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         priority = 1000,
         config = function()
             require("monokai-pro").setup({
-                transparent_background = true,
+                transparent_background = false,
                 terminal_colors = true,
                 devicons = true
             })
@@ -16,10 +17,32 @@ return {
     },
     {
         "craftzdog/solarized-osaka.nvim",
+        enabled = false,
         lazy = false,
         priority = 1000,
         config = function ()
+            require('solarized-osaka').setup({transparent = false})
             vim.cmd([[colorscheme solarized-osaka]])
         end
+    },
+    {
+        'Tsuzat/NeoSolarized.nvim',
+        enabled = false,
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('NeoSolarized').setup({ style = "light", transparent = false })
+            vim.cmd([[colorscheme NeoSolarized]])
+        end
+    },
+    {
+        'catppuccin/nvim',
+        lazy = false,
+        priority = 1000,
+        name = 'catppuccin',
+        config = function ()
+            vim.cmd([[colorscheme catppuccin-latte]])
+        end
+
     }
 }
