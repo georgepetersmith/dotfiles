2119de5e97331fa91d9c0ee6fd8baaec3b3877e1
George Smith <georgepetersmith@outlook.com>
2024-03-15 19:40:22 +0000

    Refine catppuccin theme for nvim and tmux

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,16 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = true
+    opts = {
+        options = {
+            component_separators = '',
+            section_separators = { left = '', right = ''},
+        },
+        extensions = {
+            'quickfix',
+            'trouble',
+            'lazy',
+            'mason',
+        }
+    }
 }
