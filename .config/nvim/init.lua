e3fec012e239d8e6a1129fccba0e292c5610692a
George Smith <georgepetersmith@outlook.com>
2024-09-14 17:47:17 +0100

    Set shiftwidth to 2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,8 +21,8 @@ vim.opt.smartcase = true
 vim.opt.wrap = false
 vim.opt.fillchars = { eob = " " }
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.fileencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
 vim.opt.pumheight = 10
