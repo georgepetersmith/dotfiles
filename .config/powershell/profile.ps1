95d29fd2cd52c0991fe534a55f9a3cef5d6d673a
George Smith <georgepetersmith@outlook.com>
2024-09-26 18:12:28 +0100

    Initialise starship

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -8,6 +8,7 @@ function dotfiles { git --git-dir=$HOME\.cfg\ --work-tree=$HOME $args }
 function lazydot { lazygit --git-dir=$HOME\.cfg\ --work-tree=$HOME }
 
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+Invoke-Expression (&starship init powershell)
 
 Set-Alias ls eza
 Set-Alias cat bat
