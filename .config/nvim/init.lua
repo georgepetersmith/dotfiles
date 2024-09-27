48d9c87f08b233863716cce0ec2a764c16df46f4
George Smith <georgepetersmith@outlook.com>
2024-09-27 18:12:15 +0100

    Replace telescope with fzf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,8 +111,28 @@ require('lazy').setup({
     end,
   },
   {
+    "ibhagwan/fzf-lua",
+    config = true,
+    keys = {
+      { '<leader>f', '<cmd>FzfLua files<cr>' },
+      { '<leader>/', '<cmd>FzfLua live_grep<cr>' },
+      { '<leader>b', '<cmd>FzfLua buffers<cr>' },
+      { '<leader>h', '<cmd>FzfLua help_tags<cr>' },
+      { '<leader>;', '<cmd>FzfLua resume<cr>' },
+      { '<leader>i', '<cmd>FzfLua lsp_finder<cr>' },
+      { '<leader>gr', '<cmd>FzfLua lsp_references<cr>' },
+      { '<leader>gr', '<cmd>FzfLua lsp_implementations<cr>' },
+      { '<leader>s', '<cmd>FzfLua lsp_document_symbols<cr>' },
+      { '<leader>S', '<cmd>FzfLua lsp_workspace_symbols<cr>' },
+      { '<leader>d', '<cmd>FzfLua lsp_document_diagnostics<cr>' },
+      { '<leader>D', '<cmd>FzfLua lsp_workspace_diagnostics<cr>' },
+      { 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>' }
+    },
+  },
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
+    enabled = false,
     opts = {
       defaults = {
         path_display = { 'smart' },
